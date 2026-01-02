9220e8aaa7c2e83446584f0c9b33019eb604de62
Mukunda <cmukunda95@gmail.com>
2026-01-02 17:55:47 +0545

    setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+// server.js
+import dotenv from 'dotenv';
+import express from 'express';
+import next from 'next';
+
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/api/hello', (req, res) => {
+    res.json({ message: 'Hello from Express!' });
+  });
+
+  server.use((req, res) => handle(req, res));
+
+  server.listen(port, () => {
+    console.log(`> Server running on http://localhost:${port}`);
+  });
+});
